3e7041cf2e58c154bc71f4f8766caa4bd2473c6e
mikemeding <mikeymeding@gmail.com>
2015-02-11 18:16:38 -0500

    just getting started

---
+++ rpiweather/displayTime.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
+import time
+from time import strftime
+
+class DisplayTime:
+    lcd = None
+
+    def __init__(self, lcd):
+        self.lcd = lcd
+
+    @staticmethod
+    def run(seconds):
+        DisplayTime.lcd.clear()
+        while seconds > 0:
+            DisplayTime.lcd.home()
+            DisplayTime.lcd.message(strftime("%H:%M:%S") + '\n' + strftime("%Y-%m-%d"))
+            time.sleep(0.8)  # Print a two line message
+            seconds = (seconds - 1)
+
+
+# Demo showing the cursor.
+#lcd.show_cursor(True)
+#lcd.blink(True)
+
+# Demo scrolling message right/left.
+#lcd.clear()
+#message = 'Scroll'
+#lcd.message(message)
+#for i in range(lcd_columns-len(message)):
+#	time.sleep(0.5)
+#	lcd.move_right()
+#for i in range(lcd_columns-len(message)):
+#	time.sleep(0.5)
+#	lcd.move_left()
---
+++ rpiweather/startDisplay.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+__author__ = 'mike'
+
+import Adafruit_CharLCD as LCD
+import displayTime as DisplayTimeClass
+
+
+# Raspberry Pi pin configuration:
+lcd_rs = 22  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en = 27
+lcd_d4 = 17
+lcd_d5 = 24
+lcd_d6 = 23
+lcd_d7 = 18
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+view_time = DisplayTimeClass.DisplayTime(lcd)
+
+# while True :
+view_time.run(60)
