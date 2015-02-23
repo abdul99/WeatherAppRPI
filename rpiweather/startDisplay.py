a3cc237de43604d6710a311d9f35271a940fde36
mike meding <mikeymeding@gmail.com>
2015-02-22 19:32:25 -0500

    working on buttons

--- rpiweather/startDisplay.py
+++ rpiweather/startDisplay.py
@@ -24,24 +24,48 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 
 # Initialize LEDs and buttons
+# MUST USE BCM SCHEME FOR P5 HEADER
 GPIO.setmode(GPIO.BCM)
 
+# p5 header pins on the rpi
 led1 = 29
 led2 = 28
-GPIO.setup(led1,GPIO.OUT)
-GPIO.setup(led2,GPIO.OUT)
+btn1 = 30
+btn2 = 31
+# LEDS are OUTPUTS
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
 
-GPIO.output(led1,True) ## turn on by default
-GPIO.output(led2,True)
+# BTNS are INPUTS. they are setup to pull down
+GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(btn2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# setup RISING edge listeners (button released)
+GPIO.add_event_detect(btn1, GPIO.RISING)
+GPIO.add_event_detect(btn2, GPIO.RISING)
+
+# GPIO.output(led1,True) ## turn on by default
+# GPIO.output(led2,True)
+
 
 # Display objects
 view_time = DisplayTime(lcd)
 view_weather = DisplayWeather(lcd)
 view_info = DisplayInfo(lcd)
 
+led1ON = True
+led2ON = True
 # This pages through the different screen info pages
 while True:
-    view_time.run(10)
-    view_weather.run()
-    view_info.run(10)
+    # view_time.run(10)
+    # view_weather.run()
+    # view_info.run(10)
+    if GPIO.event_detect(btn1):
+        led1ON = not (led1ON)
+    if GPIO.event_detect(btn2):
+        led1ON = not (led2ON)
+
+    GPIO.output(led1, led1ON)
+    GPIO.output(led2, led2ON)
+
 
